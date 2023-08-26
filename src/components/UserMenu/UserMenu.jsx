c5dc4e37c9e431ab6b94f1b53cd8701b9337c514
EvgenBatt <evgenmgid@gmail.com>
2023-08-26 13:33:08 +0300

    Update styles UserMenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,17 +1,18 @@
-import { useAuth } from "hooks/useAuth";
-import { useDispatch } from "react-redux";
-import { logOut} from "redux/operations";
+import { useAuth } from 'hooks/useAuth';
+import { useDispatch } from 'react-redux';
+import { logOut } from 'redux/operations';
+import { Button, Text, Wrap } from './UserMenu.styled';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
 
   return (
-    <>
-      <p>Welcome, {user.name}</p>
-      <button type="button" onClick={() => dispatch(logOut())}>
+    <Wrap>
+      <Text>Welcome, {user.name}</Text>
+      <Button type="button" onClick={() => dispatch(logOut())}>
         Logout
-      </button>
-    </>
+      </Button>
+    </Wrap>
   );
 };
---
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+export const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const Text = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+  color: #e7e7e7;
+`;
+
+export const Button = styled.button`
+  font-size: 18px;
+  font-weight: 500;
+  color: #e7e7e7;
+  transition-property: color, background-color, transform;
+  transition-duration: 0.2s;
+  transition-timing-function: 0.25s cubic-bezier(0.7, 0.98, 0.86, 0.98);
+  transform: scale(0.9);
+
+  &:hover,
+  &:focus {
+    color: #ff3842;
+    background-color: transparent;
+  }
+
+  &.active {
+    color: #ff3842;
+    background-color: #ffffffe3;
+
+    padding: 6px 12px;
+    border-radius: 4px;
+
+    transform: scale(1);
+    pointer-events: none;
+  }
+`;
