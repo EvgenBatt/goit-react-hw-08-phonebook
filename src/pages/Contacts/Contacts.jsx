984b0870307644189eabb56bc83583f30d9e63ab
EvgenBatt <evgenmgid@gmail.com>
2023-08-27 13:07:44 +0300

    Update Contacts useEffect

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,11 +1,18 @@
 import { Toaster } from 'react-hot-toast';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectContacts } from 'redux/contacts/selectors';
 import { EmptyContact, Flex } from './Contacts.styled';
 import { ContactForm, ContactList, Filter } from 'components';
+import { fetchContacts } from 'redux/operations';
+import { useEffect } from 'react';
 
 const Contacts = () => {
   const contacts = useSelector(selectContacts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <>
