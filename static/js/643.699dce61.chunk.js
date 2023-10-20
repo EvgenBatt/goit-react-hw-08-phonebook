e9c3df9453f6f5f3e742efbd022173b5a1c82891
EvgenBatt <evgenmgid@gmail.com>
2023-10-20 14:05:58 +0000

    Deploying to gh-pages from @ EvgenBatt/goit-react-hw-08-phonebook@eeb39cc186af2e5975f80e9a4e508c66ce49bd9a 🚀

---
+++ static/js/643.699dce61.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[643],{5643:function(e,n,r){r.r(n),r.d(n,{Register:function(){return h}});var t=r(7679),s=r(5218),c=r(815),i=r(184),h=function(){return(0,i.jsx)(i.Fragment,{children:(0,i.jsx)("main",{children:(0,i.jsx)(c.$0,{children:(0,i.jsxs)(c.W2,{children:[(0,i.jsx)(s.x7,{}),(0,i.jsx)(t.B2,{})]})})})})};n.default=h}}]);
+//# sourceMappingURL=643.699dce61.chunk.js.map
